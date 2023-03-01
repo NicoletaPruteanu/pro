6e6d2d1240ae2a8ef0776f588f6ac8ebd9250f81
NicoletaPruteanu <114097145+NicoletaPruteanu@users.noreply.github.com>
2023-03-01 20:16:09 +0200

    Add files via upload

---
+++ My project/index (1).php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Clinica D.A.M.E Landing Page</title>
+    <!-- Ion Icons Js -->
+    <script type="module" src="https://clinicadame.com/wp-content/uploads/2022/04/CLINICA-DAME-QUITO.jpg"></script>
+    <script nomodule src="h"></script>
+    <!-- JS -->
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <!-- Menu -->
+    <div class="menu">
+        <div class="container flex">
+            <!-- Mobile Button -->
+            <div class="mobile-btn">
+                <ion-icon name="grid"></ion-icon>
+            </div>
+            <div class="logo">
+                <img src="https://clinicadame.com/wp-content/uploads/2021/03/clinica-dame.png" alt="" />
+            </div>
+
+            <ul class="nav">
+                <li class="nav-item"><a href="#">Home</a></li>
+                <li class="nav-item"><a href="#why-us">Cine suntem?</a></li>
+                <li class="nav-item"><a href="#explore">Vezi mai multe</a></li>
+                <li class="nav-item"><a href="#discount">Registreazate</a></li>
+            </ul>
+
+            <a href="#" class="btn">Registreazate</a>
+        </div>
+    </div>
+    <!-- End Menu -->
+
+    <!-- Header -->
+    <header class="header">
+        <div class="container flex">
+            <div class="text">
+                <h1 class="mb">
+                    Clinica Dame <br />
+                De inestigatii cea mai buna
+                </h1>
+
+                <p class="mb">
+                    Doresti sa iti faci o investigatie medicala la cei mai buni specialisti?
+                    atunci nu ezita si inscriete la noi 
+                </p>
+
+                <a href="#" class="btn mt">Afla mai multe</a>
+            </div>
+
+            <div class="visual">
+                <img src="https://clinicadame.com/wp-content/uploads/2022/04/CLINICA-DAME-QUITO.jpg" alt="" />
+            </div>
+        </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- Why Us -->
+    <div class="section" id="why-us">
+        <div class="container flex">
+            <div class="text">
+                <h2 class="primary mb">Cine suntem?</h2>
+                
+                <p class="tertiary">
+                    
+Suntem o Instituție Privată creată pentru a oferi Servicii de Sănătate într-o manieră eficientă și responsabilă, angajați față de populația ecuadoriană și urmărind să colaboreze cu Politicile de Sănătate atât în ​​procesele de Prevenire, cât și de tratament în toate specialitățile Medicale, funcționează și oferă îngrijire neîntreruptă 24/7. ore din zi, cu servicii de ambulatoriu, spitalizare, terapie intensivă neonatală, pediatrică și adulți, diagnostic imagistic și suport de laborator.
+                </p>
+            </div>
+            <div class="visual">
+                <img src="https://www.hevcon.com.br/wp-content/uploads/2022/02/017e6206-5f56-4546-83a6-87af09bdb463.jpg" alt="" />
+            </div>
+        </div>
+    </div>
+    <!-- End Why Us -->
+
+    <!-- Explore -->
+    <div class="section" id="explore">
+        <div class="container flex">
+            <div class="visual">
+                <img src="https://medijobs.ro/wp-content/uploads/2021/02/Copie-a-blog-design-2021-02-12T153438.414.png" alt="" />
+            </div>
+            <div class="text">
+                <h2 class="primary mb">
+                   Serviciile noastre
+                </h2>
+                <p class="tertiary mb">
+                   <br>Urgenta</br>
+                   <br>Laborator</br>
+                   <br>Spitalizare</br>
+                   <br>centru de gastroenterologie</br>
+                   <br>Terapie intensiva</br>
+
+                
+
+                </p>
+
+                
+            </div>
+        </div>
+    </div>
+    <!-- End Explore -->
+
+    <!-- Trainer -->
+    <div class="section" id="trainer">
+        <h2 class="primary mb">Valorile noastre principale</h2>
+        <div class="container flex">
+            <div class="trainer">
+                <img src="https://clinicadame.com/wp-content/uploads/2022/04/CLINICA-DAME-ETICA.png" width="120
+                " alt="" />
+                <h3 class="secondary mb">Alan Smith</h3>
+                <p class="tertiary mb">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla.
+                </p>
+
+                <a href="#" class="btn-2">
+                    <ion-icon name="arrow-redo-circle-outline"></ion-icon>
+                </a>
+            </div>
+
+            <div class="trainer">
+                <img src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/trainer2.jpg" alt="" />
+                <h3 class="secondary mb">Alan Smith</h3>
+                <p class="tertiary mb">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla.
+                </p>
+
+                <a href="#" class="btn-2">
+                    <ion-icon name="arrow-redo-circle-outline"></ion-icon>
+                </a>
+            </div>
+
+            <div class="trainer">
+                <img src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/trainer3.jpg" alt="" />
+                <h3 class="secondary mb">Alan Smith</h3>
+                <p class="tertiary mb">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla.
+                </p>
+
+                <a href="#" class="btn-2">
+                    <ion-icon name="arrow-redo-circle-outline"></ion-icon>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- End Trainer -->
+
+    <!-- Testimonial -->
+    <div class="section" id="testimonial">
+        <div class="container flex">
+            <div class="text">
+                <h2 class="primary">
+                    That's What Our Super <br />
+                    Client Says
+                </h2>
+
+                <br />
+                <br />
+                <br />
+
+                <div class="client">
+                    <img src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/client1.jpg" alt="" />
+                    <h2 class="secondary">Exelent Training</h2>
+                    <p class="tertiary">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+                        quas voluptatem ad, repudiandae voluptates odio deleniti
+                        reiciendis in veniam quidem expedita maxime error fugit. Pariatur
+                        quasi sunt aut id. Lorem, ipsum dolor sit amet consectetur
+                        adipisicing elit. Neque, officiis.
+                    </p>
+                </div>
+            </div>
+            <div class="visual">
+                <img src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/testimonial.png" alt="" />
+            </div>
+        </div>
+    </div>
+    <!-- End Testimonial -->
+
+    <!-- Discount -->
+    <div class="section" id="discount">
+        <div class="container flex">
+            <div class="visual">
+                <img src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/discount.png" alt="" />
+            </div>
+            <div class="text">
+                <h2 class="primary mb">
+                    Fitness Classes This Summer, Pay Now And Get 25% Discount
+                </h2>
+
+                <p class="tertiary mb">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab maxime
+                    minus praesentium est et veniam voluptate alias excepturi minima
+                    placeat amet nostrum, eligendi, quod cum ducimus nesciunt ipsa eum,
+                    explicabo eaque obcaecati.
+                </p>
+
+                <a href="#" class="btn bt">Book Now</a>
+            </div>
+        </div>
+    </div>
+    <!-- End Discount -->
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container flex">
+            <p class="tertiary">
+                &copy; 2022 Programmer Cloud. All Rights Reserved.
+            </p>
+        </div>
+    </footer>
+
+    <!-- End Footer -->
+    <script src="script.js"></script>
+</body>
+
+</html>
